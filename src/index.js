d7ace4b30347c47d77bbf53a0f326ca981382087
wanghui <wanghui@wanghuideiMac.local>
2019-01-15 16:16:26 +0800

    login

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import ReactDOM from "react-dom";
 import data from "./data.json";
 import "./styles.css";
 import Board from "react-trello";
+import LoginPanel from "./containers/LoginPanel";
 
 function App() {
   return (
     <div className="App">
       <h1>用户故事地图</h1>
-      <Board data={data} draggable />
+      {/* <Board data={data} draggable /> */}
+      <LoginPanel />
     </div>
   );
 }
