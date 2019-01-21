a95a851f2ac4e2eee8c00d1bc73b52b7102392f8
王卉 <wanghui@wanghuideMacBook-Pro.local>
2019-01-22 00:00:42 +0800

    :develop:加监听

--- src/containers/storyMap/StoryMap.jsx
+++ src/containers/storyMap/StoryMap.jsx
@@ -5,13 +5,29 @@ import Board from 'react-trello';
 
 
 export default class StoryMap extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {};
+      }
+    onDataChange = (e) => {
+        console.log('datachange',e)
+    }
+    onCardDelete= (e) => {
+        console.log('delete',e)
+    }
+    onCardAdd= (e) => {
+        console.log('add',e)
+    }
+    onCardClick= (e) => {
+        console.log('click',e)
+    }
     render(){
         return (
             <Board data={data} 
-            // onDataChange={onDataChange}
-            // onCardDelete={onCardDelete}
-            // onCardAdd={onCardAdd}
-            // onCardClick={onCardClick}
+            onDataChange={this.onDataChange}
+            onCardDelete={this.onCardDelete}
+            onCardAdd={this.onCardAdd}
+            onCardClick={this.onCardClick}
             editable
             draggable
             />
